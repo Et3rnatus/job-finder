37dbed1d1074a0a60f47cfae5cfff53af5abff5f
Et3rnatus <thinhnetpro15@gmail.com>
2025-12-26 11:16:59 +0700

    sua loi SQL

--- BE/src/controllers/job.controller.js
+++ BE/src/controllers/job.controller.js
@@ -2,12 +2,12 @@ const pool = require('../config/db');
 const {v4: uuidv4} = require('uuid');
 
 exports.createJob = async (req, res) => {
-    const {empolyer_id,title,description,min_salary,max_salary,location,emloyment_type} = req.body;
+    const {employer_id,title,description,min_salary,max_salary,location,employment_type} = req.body;
 
     try {
         await pool.query(
-            'INSERT INTO jobs (id, empolyer_id, title, description, min_salary, max_salary, location, emloyment_type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-            [uuidv4(), empolyer_id, title, description, min_salary, max_salary, location, emloyment_type]
+            'INSERT INTO job (id, employer_id, title, description, min_salary, max_salary, location, employment_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [uuidv4(), employer_id, title, description, min_salary, max_salary, location, employment_type]
         );
         res.json({message: 'Job created successfully'});
     }
@@ -19,7 +19,7 @@ exports.createJob = async (req, res) => {
 exports.getJobs = async (req, res) => {
     try {
         const [rows] = await pool.query(
-            'SELECT j.id, j.title, j.location, j.min_salary, j.max_salary,e.company_nameFROM job j JOIN employer e ON j.empolyer_id = e.id ORDER BY j.created_at DESC');
+            'SELECT j.id, j.title, j.location, j.min_salary, j.max_salary,e.company_name FROM job j JOIN employer e ON j.employer_id = e.id ORDER BY j.created_at DESC');
         res.json(rows);
     }
     catch (err) {
--- BE/src/routes/job.routes.js
+++ BE/src/routes/job.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const jobController = require('../controllers/job.controller');
 
-router.post('/jobs', jobController.createJob);
-router.get('/jobs', jobController.getJobs);
+router.post('/', jobController.createJob);
+router.get('/', jobController.getJobs);
 router.get('/:id', jobController.getJobDetail);
 
 module.exports = router;
